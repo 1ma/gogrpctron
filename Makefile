0292a4399032df5d92314a9a34886ae6245e64ca
Marcel Hernandez <e6990620@gmail.com>
2021-09-19 17:15:42 +0200

    implement lame client

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
-.PHONY: dev mysql clean
+.PHONY: server client mysql clean protoc
 
-dev:
-	protoc --proto_path=/usr/local/include --proto_path=internal/grpc --go_out=. --go-grpc_out=. internal/grpc/user_service.proto
+server: protoc
 	docker-compose run --rm starter
 	go run cmd/server/main.go
+
+client: protoc
+	go run cmd/client/main.go
 
 mysql:
 	docker-compose exec mysql mysql -D gogrpctron
 
 clean:
 	rm -f internal/grpc/*.pb.go
+
+protoc:
+	protoc --proto_path=/usr/local/include --proto_path=internal/grpc --go_out=. --go-grpc_out=. internal/grpc/user_service.proto
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	pb "1mahq.com/gogrpctron/internal/grpc"
+)
 
 func main() {
-	fmt.Println("client program")
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUserServiceClient(conn)
+
+	response, err := c.ListUsers(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %v", response)
 }
