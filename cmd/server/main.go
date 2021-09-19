2be123dd3c41575ad626c53ce465e32937fa17cd
Marcel Hernandez <e6990620@gmail.com>
2021-09-19 16:52:50 +0200

    implement fake server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	pb "1mahq.com/gogrpctron/internal/grpc"
+)
+
+type userService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (s *userService) CreateUser(ctx context.Context, in *emptypb.Empty) (*pb.UserData, error) {
+	return &pb.UserData{Id: 1, UserName: "Christiana Hessel", CreatedAt: timestamppb.Now()}, nil
+}
+
+func (s *userService) ListUsers(ctx context.Context, in *emptypb.Empty) (*pb.UserList, error) {
+	return &pb.UserList{Users: []*pb.UserData{{Id: 1, UserName: "Christiana Hessel", CreatedAt: timestamppb.Now()}}}, nil
+}
 
 func main() {
-	fmt.Println("server program")
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("server listening at %v", listener.Addr())
+
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &userService{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
